59368c3e028249ede3ddb6f318144a012a0061db
nethsaragimhan <94338947+nethsaragimhan@users.noreply.github.com>
2022-04-04 20:12:03 +0530

    Deploy error fixed

---
+++ qr.js
@@ -0,0 +1,58 @@
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+async function whatsAsena() {
+  const conn = new WAConnection();
+  conn.logger.level = 'warn';
+  conn.version = [2, 2126, 14]
+
+  conn.on('connecting', async () => {
+    console.log(`${chalk.green.bold('King')}${chalk.green.bold('Bot')}
+${chalk.white.italic('King String code recipient')}
+${chalk.blue.bold('ℹ️  Connecting Whatsapp... Please wait.')}`);
+  });
+
+  conn.on('open', async () => {
+    console.log(
+      chalk.green.bold('KING-BOT QR Code: '),
+      'KINGBOT;;;' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      )
+    );
+    await conn.sendMessage(
+      conn.user.jid,
+      'KINGBOT;;;' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      ),
+      MessageType.text
+    );
+    if (conn.user.jid.startsWith('91')) {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ KING Scanned✅️*\n*▪️Thanks For Choosing KING Bot ✌️*',
+        MessageType.text
+      );
+    } else {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ KING Qr Successfully Scanned✅️*\n*▪️Thanks For Choosing KING Bot ✌️*',
+        MessageType.text
+      );
+    }
+    console.log(
+      chalk.green.bold(
+        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
+      ),
+      chalk.green.bold(
+        'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+      )
+    );
+    process.exit(0);
+  });
+
+  await conn.connect();
+}
+
+whatsAsena();
